3ff6a56f1a1ff872cb1ec7f717e01bf9067f6e9a
dolgopyat <ajsdfhkljasbfgvm,d@bjvnb>
2022-10-14 22:21:17 +0300

    Всё работает

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Author levTolstoy = new Author("Лев", "Толстой");
         Book voynaIMir = new Book("Война и Мир", levTolstoy, 2022);
+        Book voynaIMir2 = new Book("Война и Мир", levTolstoy, 2022);
         System.out.println(voynaIMir.bookInfo());
+        System.out.println(voynaIMir);
+        System.out.println(voynaIMir.hashCode() == voynaIMir2.hashCode());
+        System.out.println(voynaIMir.equals(voynaIMir2));
     }
 }
